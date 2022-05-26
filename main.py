211437cc45e2ccbeb86cbbada8975c427fbc4806
RIchard Oluwadamilola Oluwatobilola <oluwatobyrichard@gmail.com>
2022-05-26 14:52:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(sorted(word) == sorted(anagram)):
+       return True
+    return False
+
+print(find_anagram("keen", "knee"))
